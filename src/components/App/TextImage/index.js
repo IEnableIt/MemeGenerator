1d97b7b294f8633b5c6a399c6bc9c0b753323461
Nobert Patrick <ayesiganobert@gmail.com>
2023-11-28 01:35:16 +0300

    fix: handle positioning

--- src/components/App/TextImage/index.js
+++ src/components/App/TextImage/index.js
@@ -68,6 +68,80 @@ const TextImage = () => {
         />
       </WrapInput>
 
+      <WrapInput flex>
+        <div className={meme.state.textOutside ? 'inactive' : ''}>
+          <Label htmlFor='pos-top'>
+            Text position <span>[ {meme.state.topTextPos} ]</span>
+          </Label>
+          <Range
+            id='pos-top'
+            min='0'
+            max='50'
+            value={meme.state.topTextPos}
+            disabled={!meme.state.imageSelected || meme.state.textOutside}
+            onChange={(e) => handleTextPos(e, 'top')}
+          />
+        </div>
+        <div>
+          <Label htmlFor='size-top'>
+            Text size <span>[ {meme.state.topTextSize} ]</span>
+          </Label>
+          <Range
+            id='size-top'
+            min='1'
+            max='4'
+            step='0.1'
+            value={meme.state.topTextSize}
+            disabled={!meme.state.imageSelected}
+            onChange={(e) => handleTextSize(e, 'top')}
+          />
+        </div>
+      </WrapInput>
+
+      {/* Bottom Text */}
+      <WrapInput>
+        <Label primary htmlFor='text-bottom'>
+          Bottom text
+        </Label>
+        <Input
+          intype='text'
+          id='text-bottom'
+          onChange={handleBottomText}
+          value={meme.state.bottomText}
+          disabled={!meme.state.imageSelected}
+        />
+      </WrapInput>
+
+      <WrapInput flex>
+        <div className={meme.state.textOutside ? 'inactive' : ''}>
+          <Label htmlFor='pos-bottom'>
+            Text position <span>[ {meme.state.bottomTextPos} ]</span>
+          </Label>
+          <Range
+            id='pos-bottom'
+            min='0'
+            max='50'
+            value={meme.state.bottomTextPos}
+            disabled={!meme.state.imageSelected || meme.state.textOutside}
+            onChange={(e) => handleTextPos(e, 'bottom')}
+          />
+        </div>
+        <div>
+          <Label htmlFor='size-bottom'>
+            Text size <span>[ {meme.state.bottomTextSize} ]</span>
+          </Label>
+          <Range
+            id='size-bottom'
+            min='1'
+            max='4'
+            step='0.1'
+            value={meme.state.bottomTextSize}
+            disabled={!meme.state.imageSelected}
+            onChange={(e) => handleTextSize(e, 'bottom')}
+          />
+        </div>
+      </WrapInput>
+
       <TextLegenda>* Both of the above texts are optional.</TextLegenda>
     </TextWrapper>
   )
