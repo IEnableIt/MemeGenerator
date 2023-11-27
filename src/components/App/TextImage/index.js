4f0e5a7875cc1825419753a2a5d60da5d8262cbd
IEnableIt <111333950+IEnableIt@users.noreply.github.com>
2023-11-28 01:17:16 +0300

    src code added

---
+++ src/components/App/TextImage/index.js
@@ -0,0 +1,47 @@
+import React, { useContext } from 'react'
+import { MemeContext } from '../../../context/MemeContext'
+import TextWrapper from './TextWrapper'
+import TextLegenda from './TextLegenda'
+import Title from '../../global/Title'
+import WrapInput from '../../global/form/WrapInput'
+import Label from '../../global/form/Label'
+import Input from '../../global/form/Input'
+import Range from '../../global/form/Range'
+import Switch from '../../global/form/Switch'
+
+const TextImage = () => {
+  // Global state
+  // state to read and dispatch to modify
+  const meme = useContext(MemeContext)
+
+  const handleTopText = (e) => {
+    meme.dispatch({ type: 'UPDATE_TOP', payload: e.target.value })
+  }
+
+  // Render
+  return (
+    <TextWrapper className={meme.state.imageSelected ? 'active' : ''}>
+      <Title as='h3' fsize='1.5' margin='0 0 1rem'>
+        Set your text here
+      </Title>
+
+      {/* Top Text */}
+      <WrapInput>
+        <Label primary htmlFor='text-top'>
+          Top text
+        </Label>
+        <Input
+          intype='text'
+          id='text-top'
+          onChange={handleTopText}
+          value={meme.state.topText}
+          disabled={!meme.state.imageSelected}
+        />
+      </WrapInput>
+
+      <TextLegenda>* Both of the above texts are optional.</TextLegenda>
+    </TextWrapper>
+  )
+}
+
+export default TextImage
